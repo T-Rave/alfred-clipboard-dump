e306b067ca469c5476b3cff9869ab27f2e0718a1
T-Rave Flywheel <t-rave@T-Raves-MacBook-Pro.local>
2018-05-04 12:47:19 -0500

    sqlite output change from -csv to -list for cleaner data

--- clipdump-workflow.sh
+++ clipdump-workflow.sh
@@ -9,7 +9,7 @@ else
 	result="ORDER BY item DESC LIMIT $limit"
 fi
 
-sqlite3 -csv ~/Library/Application\ Support/Alfred\ 3/Databases/clipboard.alfdb "SELECT item FROM clipboard $result;" > clipdump.txt
+sqlite3 -list ~/Library/Application\ Support/Alfred\ 3/Databases/clipboard.alfdb "SELECT item FROM clipboard $result;" > clipdump.txt
 
 pbcopy < ./clipdump.txt
 > clipdump.txt
