4f20d2485c1f25440acd0c83521a5e8ff381d75f
T-Rave Flywheel <t-rave@T-Raves-MacBook-Pro.local>
2018-05-04 07:09:50 -0500

    script for use in Workflow

---
+++ clipdump-workflow.sh
@@ -0,0 +1,14 @@
+limit=$1
+
+> clipdump.txt
+
+if [ "$limit" = "export" ] 
+then
+	result=""
+else
+	result="LIMIT $limit"
+fi
+
+sqlite3 -csv ~/Library/Application\ Support/Alfred\ 3/Databases/clipboard.alfdb "SELECT item FROM clipboard $result;" > clipdump.txt
+
+pbcopy < ./clipdump.txt
