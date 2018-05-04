0d7abd81898583a7d2f562cd39202a291919a025
T-Rave Flywheel <t-rave@T-Raves-MacBook-Pro.local>
2018-05-04 11:10:04 -0500

    better way to output the last n clipboard items

--- clipdump-workflow.sh
+++ clipdump-workflow.sh
@@ -6,12 +6,10 @@ if [ "$limit" = "export" ]
 then
 	result=""
 else
-	result="LIMIT $limit"
+	result="ORDER BY item DESC LIMIT $limit"
 fi
 
 sqlite3 -csv ~/Library/Application\ Support/Alfred\ 3/Databases/clipboard.alfdb "SELECT item FROM clipboard $result;" > clipdump.txt
-
-tail -r ./clipdump.txt
 
 pbcopy < ./clipdump.txt
 > clipdump.txt
